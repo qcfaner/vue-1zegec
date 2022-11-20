3c87233a9a93677fff7460ea9acb186f1f0875fb
qcfaner <qcfaner@outlook.com>
2022-11-20 10:58:30 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# vue-1zegec
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vue-1zegec)
---
+++ src/main.js
@@ -0,0 +1,7 @@
+const { createApp } = require('vue');
+import ElementPlus from 'element-plus';
+import 'element-plus/dist/index.css';
+
+import App from './App.vue';
+
+createApp(App).use(ElementPlus).mount('#app');
